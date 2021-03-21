a53d750d48b846928aee8b291e8aecc209c82fdd
utibori-jp <kouki.utibori@gmail.com>
2021-03-21 14:51:24 +0900

    changed reservasion_system.py

--- reservation_system.py
+++ reservation_system.py
@@ -11,6 +11,10 @@ class Patient(Human):
     def show_data(self):
         print(self.name, self.symptom, self.patient_id)
 
+    #objectを指定して、そのままpinrtするときの書式を指定できる。line44のような書き方をせずとも、情報をprint出来る
+    def __str__ (self):
+        return f"{self.name} {self.symptom}{self.patient_id}"
+
 
 class Clinic:
     def __init__ (self):
@@ -19,32 +23,26 @@ class Clinic:
     def show_waiting_count(self):
         print("now waiting {} people".format(len(self.reservation_list)))
 
-    def check_reserved(self, name):
+    def check_reserved(self, name:Patient):
         for i in range(len(self.reservation_list)):
             if self.reservation_list[i] == name:
                 return True
             else:
                 return False
 
-    def reserve(self, name):
+    def reserve(self, name:Patient):
         if self.check_reserved(name):
-            print("you'veb already reserved")
+            print("you've already reserved")
         else:
             self.reservation_list.append(name)
             print("your reservation is done")
 
-    def diagnosis(self, name = None):
+    def diagnosis(self, name:Patient = None):
         if (name):
             print("ok!")
         else:
-            #koredato <__main__.Patient object at 0x0000024C5DBD0A90> mitainanogaderu
-            # print(self.reservation_list)
+            self.reservation_list[0].show_data()
             self.reservation_list.pop(0)
-
-    @staticmethod
-    def run ():
-        pass
-
 
 
 def main():
@@ -71,7 +69,8 @@ def main():
     myclinic.show_waiting_count()
     # print(type(myclinic.reservation_list))
 
-    Clinic.run()
+    #__str__を用いたprint方法
+    print (sato)
 
 
 if __name__ == "__main__":
